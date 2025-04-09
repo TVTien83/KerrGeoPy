3f353b13f641094dada2afc544219035ec2bde7a
tvtien <tvtien832@gmail.com>
2025-04-09 03:15:53 +0100

    improved solver and multiple functions, added phi-theta ratio in the limit cases, added function for finding r-mode of triple resonances.

--- kerrgeopy/resonance.py
+++ kerrgeopy/resonance.py
@@ -4,7 +4,7 @@ from .frequencies import *
 from .frequencies import _ellippi
 from .constants import *
 from .constants import _standardize_params
-from scipy.optimize import fsolve, newton
+from scipy.optimize import fsolve, newton, root_scalar
 from scipy.special import elliprf
 from numpy import sqrt, pi, sign
 
@@ -165,7 +165,7 @@ def _phitheta_frequencyRatio(a, p, e, x):
     h_plus = (r1 - r2) * (r3 - r_plus) / ((r1 - r3) * (r2 - r_plus))
     h_minus = (r1 - r2) * (r3 - r_minus) / ((r1 - r3) * (r2 - r_minus))
     
-    # simplified form of epsilon0*z_plus
+    #simplified form of epsilon0*z_plus
     e0zp = (a**2 * (1 - E**2) * (1 - z_minus) + L**2) / (L**2 * (1 - z_minus))
     
     #theta-r ratio
@@ -191,6 +191,42 @@ def _phitheta_frequencyRatio(a, p, e, x):
 
 # Finding resonance
 ## In general 
+
+def _doubleResonance_solver(equation, p0, sep):
+    '''Try `scipy.newton` twive with different initial guess, one of them is very near the separatrix
+    
+    Parameters
+    ----------
+    equation: callable
+        resonant condition equation needed to be solved
+    p0 : double
+        initial guess p of the first attemp
+    sep : double
+        initial guess p of the second attemp, which also is p at the separatrix
+    
+    Returns
+    -------
+    p : double
+        orbital semi-latus rectum
+    '''
+    try:
+        p_sol = newton(equation, p0)
+    except RuntimeError:
+        try:
+            p_sol = root_scalar(equation, method="brentq", bracket=(sep+1e-5, p0)).root
+        except:
+            try:
+                p_sol = newton(equation, sep+1e-5)
+            except RuntimeError:
+                pass
+            else:
+                return p_sol
+        else:
+            return p_sol
+    else:
+        return p_sol
+    raise RuntimeError("p is too close to the separatrix")
+    
 def rtheta_resonance(ratio, a, e, x):
     """Return p of r-theta resonant orbit
     
@@ -223,24 +259,10 @@ def rtheta_resonance(ratio, a, e, x):
         raise ValueError("Marginally bound orbits not supported")
     if not valid_params(a, e, x):
         raise ValueError("a^2, e and x^2 must be between 0 and 1")
-    
-    p_sol = None
-    try:
-        p0 = 6/(1-ratio**2)
-        p1 = separatrix(a, e, x)+1e-10
-        p_sol = newton(lambda p: abs(_rtheta_frequencyRatio(a, p, e, x))-ratio, x0=p0, x1=p1)
-    except ValueError:
-        try:
-            p0 = separatrix(a, e, x)+1e-10
-            p_sol = newton(lambda p: abs(_rtheta_frequencyRatio(a, p, e, x))-ratio, x0=p0)
-        except ValueError:
-            pass
-        else:
-            return p_sol    
-    else:
-        return p_sol
-    if not p_sol:
-        raise ValueError("p is too close to that of the separatrix.")
+
+    return _doubleResonance_solver(lambda p: abs(_rtheta_frequencyRatio(a, p, e, x))-ratio,
+                                   6/(1-ratio**2),
+                                   separatrix(a, e, x))
 
 def rphi_resonance(ratio, a, e, x):
     """Return p of r-phi resonant orbit
@@ -275,23 +297,9 @@ def rphi_resonance(ratio, a, e, x):
     if not valid_params(a, e, x):
         raise ValueError("a^2, e and x^2 must be between 0 and 1")
 
-    p_sol = None
-    try:
-        p0 = 6/(1-ratio**2)
-        p1 = separatrix(a, e, x)+1e-10
-        p_sol = newton(lambda p: abs(_rphi_frequencyRatio(a, p, e, x))-ratio, x0=p0, x1=p1)
-    except ValueError:
-        try:
-            p0 = separatrix(a, e, x)+1e-10
-            p_sol = newton(lambda p: abs(_rphi_frequencyRatio(a, p, e, x))-ratio, x0=p0)
-        except ValueError:
-            pass
-        else:
-            return p_sol    
-    else:
-        return p_sol
-    if not p_sol:
-        raise ValueError("p is too close to that of the separatrix.")
+    return _doubleResonance_solver(lambda p: abs(_rphi_frequencyRatio(a, p, e, x))-ratio,
+                                   6/(1-ratio**2),
+                                   separatrix(a, e, x))
 
 def phitheta_resonance(ratio, a, e, x):
     """Return p of phi-theta resonant orbit
@@ -329,23 +337,9 @@ def phitheta_resonance(ratio, a, e, x):
     if not valid_params(a, e, x):
         raise ValueError("a^2, e and x^2 must be between 0 and 1")
 
-    p_sol = None
-    try:
-        p0 = (2*a/abs(ratio**2-1))**(2/3)
-        p1 = separatrix(a, e, x)+1e-10
-        p_sol = newton(lambda p: abs(_phitheta_frequencyRatio(a, p, e, x))-ratio, x0=p0, x1=p1)
-    except ValueError: 
-        try:
-            p0 = separatrix(a, e, x)+1e-10
-            p_sol = newton(lambda p: abs(_phitheta_frequencyRatio(a, p, e, x))-ratio, x0=p0)
-        except ValueError:
-            pass
-        else:
-            return p_sol
-    else:
-        return p_sol
-    if not p_sol:
-        raise ValueError("p is too close to that of the separatrix.")
+    return _doubleResonance_solver(lambda p: abs(_phitheta_frequencyRatio(a, p, e, x))-ratio,
+                                   (2*a/abs(ratio-1))**(2/3),
+                                   separatrix(a, e, x))
 
 # Frequency ratio of r-theta and r-phi in special limits (support finding triple resonance)
 ## In the polar limit
@@ -439,6 +433,59 @@ def rphi_frequencyRatio_polarLimit(a, p, e, is_prograde=True):
     B_r = factor2*(B_r_plus-B_r_minus)
     #B_theta = 1
     return 1/(B_r+1/rtheta_ratio)
+
+def phitheta_frequencyRatio_polarLimit(a, p, e, is_prograde=True):
+    """Ratio of phi-frequency and theta-frequency in the polar limit
+    
+    Parameters
+    ----------
+    a : double
+        dimensionless spin of the black hole
+    p : double
+        orbital semi-latus rectum
+    e : double
+        orbital eccentricity
+    is_prograde : bool
+        True (default) if the orbit is prograde. Otherwise, the orbit is retrograde
+
+    Returns
+    -------
+    double
+    """
+    Lz_sign = -1+2*int(is_prograde)
+    P = (a**2*(a**4*(-1+e**2)*2+p**4+2*a**2*p*(-2+p+e**2*(2+p)))
+         )/(
+             a**4*(-1+e**2)**2+2*a**2*(1+e**2)*p**2+(-4+p)*p**3)
+    S = (2*(a**2*(-1+e**2)+p**2)**2
+         )/(
+            a**4*(-1+e**2)**2+2*a**2*(1+e**2)*p**2+(-4+p)*p**3)
+    r1 = p/(1-e)
+    r2 = p/(1+e)
+    r3 = (S+sqrt(S**2-4*P))/2
+    r4 = (S-sqrt(S**2-4*P))/2
+    E2 = 1+2*(-1+e**2)/(2*p+S-e**2*S)
+    Q = 2*p**2*P/(a**2*(2*p+S-e**2*S))
+    #L2 = 0
+    #z2 = 1
+    #z1 = Q/(1-E2)/a**2/z2
+    
+    k_r = (r1-r2)/(r1-r3)*(r3-r4)/(r2-r4)
+    k_theta = (1-E2)*a**2/Q
+    factor1 = Lz_sign*sqrt((r1-r3)*(r2-r4)*2*(1-e**2)*a**2/(2*p**2*P))
+    rtheta_ratio = factor1*ellipk(k_theta)/ellipk(k_r)
+    
+    r_plus = 1+sqrt(1-a**2)
+    r_minus = 1-sqrt(1-a**2)
+    h_r = (r1-r2)/(r1-r3)
+    h_plus = h_r*(r3-r_plus)/(r2-r_plus)
+    h_minus = h_r*(r3-r_minus)/(r2-r_minus)
+    factor2 = 2*a/(pi*(r_plus-r_minus)*sqrt((1-E2)*(r1-r3)*(r2-r4)))*2*sqrt(E2)
+    B_r_plus = r_plus/(r3-r_plus)*(ellipk(k_r)-(r2-r3)/(r2-r_plus)*_ellippi(h_plus, k_r))
+    B_r_minus = r_minus/(r3-r_minus)*(ellipk(k_r)-(r2-r3)/(r2-r_minus)*_ellippi(h_minus, k_r))
+    B_r = factor2*(B_r_plus-B_r_minus)
+    #B_theta = 1
+    return B_r*rtheta_ratio+1
+
 
 ## In the equatorial limit
 def rtheta_frequencyRatio_equatorialLimit(a, p, e, is_prograde=True):
@@ -534,16 +581,68 @@ def rphi_frequencyRatio_equatorialLimit(a, p, e, is_prograde=True):
     B_r = factor2*(B_r_plus-B_r_minus)
     return 1/(B_r+B_theta/rtheta_ratio)    
 
+def phitheta_frequencyRatio_equatorialLimit(a, p, e, is_prograde=True):
+    """Ratio of phi-frequency and theta-frequency in the equatorial limit
+    
+    Parameters
+    ----------
+    a : double
+        dimensionless spin of the black hole
+    p : double
+        orbital semi-latus rectum
+    e : double
+        orbital eccentricity
+    is_prograde : bool
+        True (default) if the orbit is prograde. Otherwise, the orbit is retrograde
+
+    Returns
+    -------
+    double
+    """
+    Lz_sign = -1+2*int(is_prograde)
+    P = 0
+    S = 2*(a**4*(-1+e**2)*p+(-4+p)*p**3+a**2*p**2*(3+e**2+p)
+           -Lz_sign*2*sqrt(a**2*p**3*(a**4*(-1+e**2)**2+(-4*e**2+(-2+p)**2)*p**2+2*a**2*p*(-2+p+e**2*(2+p))))
+           )/(
+               a**4*(-1+e**2)**2+(-4+p)**2*p**2+2*a**2*(-1+e**2)*p*(4+p)
+           )
+    P = 0
+    r1 = p/(1-e)
+    r2 = p/(1+e)
+    r3 = (S+sqrt(S**2-4*P))/2
+    r4 = (S-sqrt(S**2-4*P))/2
+    E2 = 1+2*(-1+e**2)/(2*p+S-e**2*S)
+    L2 = (2*(a**2*(-1+e**2)+p**2)*(a**2-P)+4*a**2*p*S)/(a**2*(2*p+S-e**2*S))
+    #Q = 0
+    z_minus = 0
+    z_plus = -(p**2+2*p*S)/(a**2*(-1+e**2))
+    k_r = (r1-r2)/(r1-r3)*(r3-r4)/(r2-r4)
+    k_theta = 0
+    factor1 = Lz_sign*sqrt((r1-r3)*(r2-r4)/(a**2*z_plus))
+    rtheta_ratio = factor1*ellipk(k_theta)/ellipk(k_r)
+
+    r_plus = 1+sqrt(1-a**2)
+    r_minus = 1-sqrt(1-a**2)
+    h_r = (r1-r2)/(r1-r3)
+    h_plus = h_r*(r3-r_plus)/(r2-r_plus)
+    h_minus = h_r*(r3-r_minus)/(r2-r_minus)
+    factor2 = 2*a/(pi*(r_plus-r_minus)*sqrt((1-E2)*(r1-r3)*(r2-r4)))
+    B_theta = 2*_ellippi(z_minus, k_theta)*sqrt(L2)/(pi*sqrt((1-E2)*a**2*z_plus))
+    B_r_plus = (2*sqrt(E2)*r_plus-Lz_sign*a*sqrt(L2))/(r3-r_plus)*(ellipk(k_r)-(r2-r3)/(r2-r_plus)*_ellippi(h_plus, k_r))
+    B_r_minus = (2*sqrt(E2)*r_minus-Lz_sign*a*sqrt(L2))/(r3-r_minus)*(ellipk(k_r)-(r2-r3)/(r2-r_minus)*_ellippi(h_minus, k_r))
+    B_r = factor2*(B_r_plus-B_r_minus)
+    return B_r*rtheta_ratio+B_theta
+
 # Finding triple resonance
-def tripleResonance_phiMode(rMode, thetaMode, a, e, is_prograde=True):
-    """Find possible phi-modes sastify rMode*omega_r=thetaMode*omega_theta=phiMode*omega_phi.
+def tripleResonance_rMode(thetaMode, phiMode, a, e, is_prograde=True):
+    """Find possible r-modes sastify rMode*omega_r=thetaMode*omega_theta=phiMode*omega_phi.
 
     Parameters
     ----------
     rMode : double
         radial mode
-    thetaMode : double
-        polar-angle mode
+    phiMode : double
+        azimuthal-angle mode
     a : double
         dimensionless spin of the black hole
     e : double
@@ -554,32 +653,37 @@ def tripleResonance_phiMode(rMode, thetaMode, a, e, is_prograde=True):
     Returns
     ------- 
     (double, double)
-        Boundaries of the posible phi-mode.
+        Boundaries of the posible theta-mode.
     """
         
     a = abs(a)
-    rtheta_ratio = thetaMode/rMode
+    phitheta_ratio = thetaMode/phiMode
     Lz_sign = int(is_prograde)*2-1
     
-    if not valid_frequencyRatio(rtheta_ratio):
-        raise ValueError("r-mode must be larger than theta-mode")
+    if (phitheta_ratio <= 0) | (phitheta_ratio == 1):
+        raise ValueError("The ratio must be positive and not equal to 1")
+    if (phitheta_ratio > 1) & (not is_prograde):
+        raise ValueError("The ratio must be larger than 1 if the orbit is prograde")
+    if (phitheta_ratio < 1) & is_prograde:
+        raise ValueError("The ratio must be lesser than 1 if the orbit is retrograde")
     if a == 1:
         raise ValueError("Extreme Kerr not supported")
     if e == 1:
         raise ValueError("Marginally bound orbits not supported")
     if not valid_params(a, e, 0.5):
         raise ValueError("a^2, e and x^2 must be between 0 and 1")
+
+    p1 = _doubleResonance_solver(lambda p: abs(phitheta_frequencyRatio_equatorialLimit(a, p, e, is_prograde))-phitheta_ratio,
+                                 (2*a/abs(phitheta_ratio-1))**(2/3),
+                                 separatrix(a, e, Lz_sign))
+    rtheta_ratio1 = abs(rtheta_frequencyRatio_equatorialLimit(a, p1, e, is_prograde))
     
-    p1 = newton(lambda p: abs(rtheta_frequencyRatio_equatorialLimit(a, p, e, is_prograde))-rtheta_ratio,
-                6/(1-rtheta_ratio**2))
-    p2 = newton(lambda p: abs(rtheta_frequencyRatio_polarLimit(a, p, e, is_prograde))-rtheta_ratio,
-                6/(1-rtheta_ratio**2))
-    k1 = newton(lambda rphi_ratio: abs(rphi_frequencyRatio_equatorialLimit(a, p1, e, is_prograde))-rphi_ratio,
-                rtheta_ratio)
-    k2 = newton(lambda rphi_ratio: abs(rphi_frequencyRatio_polarLimit(a, p2, e, is_prograde))-rphi_ratio,
-                rtheta_ratio)
+    p2 = _doubleResonance_solver(lambda p: abs(phitheta_frequencyRatio_polarLimit(a, p, e, is_prograde))-phitheta_ratio,
+                                 (2*a/abs(phitheta_ratio-1))**(2/3),
+                                 separatrix(a, e, 0))
+    rtheta_ratio2 = abs(rtheta_frequencyRatio_polarLimit(a, p2, e, is_prograde))
     
-    return sorted([k1*rMode, k2*rMode])
+    return sorted([thetaMode/rtheta_ratio1, thetaMode/rtheta_ratio2])
 
 def tripleResonance_thetaMode(rMode, phiMode, a, e, is_prograde=True):
     """Find possible theta-modes sastify rMode*omega_r=thetaMode*omega_theta=phiMode*omega_phi.
@@ -615,17 +719,65 @@ def tripleResonance_thetaMode(rMode, phiMode, a, e, is_prograde=True):
         raise ValueError("Marginally bound orbits not supported")
     if not valid_params(a, e, 0.5):
         raise ValueError("a^2, e and x^2 must be between 0 and 1")
+
+    p1 = _doubleResonance_solver(lambda p: abs(rphi_frequencyRatio_equatorialLimit(a, p, e, is_prograde))-rphi_ratio,
+                                 6/(1-rphi_ratio**2),
+                                 separatrix(a, e, Lz_sign))
+    rtheta_ratio1 = abs(rtheta_frequencyRatio_equatorialLimit(a, p1, e, is_prograde))
     
-    p1 = newton(lambda p: abs(rphi_frequencyRatio_equatorialLimit(a, p, e, is_prograde))-rphi_ratio,
-                6/(1-rphi_ratio**2))
-    p2 = newton(lambda p: abs(rphi_frequencyRatio_polarLimit(a, p, e, is_prograde))-rphi_ratio,
-                6/(1-rphi_ratio**2))
-    k1 = newton(lambda rtheta_ratio: abs(rtheta_frequencyRatio_equatorialLimit(a, p1, e, is_prograde))-rtheta_ratio,
-                rphi_ratio)
-    k2 = newton(lambda rtheta_ratio: abs(rtheta_frequencyRatio_polarLimit(a, p2, e, is_prograde))-rtheta_ratio,
-                rphi_ratio)
+    p2 = _doubleResonance_solver(lambda p: abs(rphi_frequencyRatio_polarLimit(a, p, e, is_prograde))-rphi_ratio,
+                                 6/(1-rphi_ratio**2),
+                                 separatrix(a, e, 0))
+    rtheta_ratio2 = abs(rtheta_frequencyRatio_polarLimit(a, p2, e, is_prograde))
     
-    return sorted([k1*rMode, k2*rMode])
+    return sorted([rtheta_ratio1*rMode, rtheta_ratio2*rMode])
+
+def tripleResonance_phiMode(rMode, thetaMode, a, e, is_prograde=True):
+    """Find possible phi-modes sastify rMode*omega_r=thetaMode*omega_theta=phiMode*omega_phi.
+
+    Parameters
+    ----------
+    rMode : double
+        radial mode
+    thetaMode : double
+        polar-angle mode
+    a : double
+        dimensionless spin of the black hole
+    e : double
+        orbital eccentricity
+    is_prograde : bool
+        True (default) if the orbit is prograde. Otherwise, the orbit is retrograde
+    
+    Returns
+    ------- 
+    (double, double)
+        Boundaries of the posible phi-mode.
+    """
+        
+    a = abs(a)
+    rtheta_ratio = thetaMode/rMode
+    Lz_sign = int(is_prograde)*2-1
+    
+    if not valid_frequencyRatio(rtheta_ratio):
+        raise ValueError("r-mode must be larger than theta-mode")
+    if a == 1:
+        raise ValueError("Extreme Kerr not supported")
+    if e == 1:
+        raise ValueError("Marginally bound orbits not supported")
+    if not valid_params(a, e, 0.5):
+        raise ValueError("a^2, e and x^2 must be between 0 and 1")
+
+    p1 = _doubleResonance_solver(lambda p: abs(rtheta_frequencyRatio_equatorialLimit(a, p, e, is_prograde))-rtheta_ratio,
+                                 6/(1-rtheta_ratio**2),
+                                 separatrix(a, e, Lz_sign))
+    rphi_ratio1 = abs(rphi_frequencyRatio_equatorialLimit(a, p1, e, is_prograde))
+    
+    p2 = _doubleResonance_solver(lambda p: abs(rtheta_frequencyRatio_polarLimit(a, p, e, is_prograde))-rtheta_ratio,
+                                 6/(1-rtheta_ratio**2),
+                                 separatrix(a, e, 0))
+    rphi_ratio2 = abs(rphi_frequencyRatio_polarLimit(a, p2, e, is_prograde))
+    
+    return sorted([rphi_ratio1*rMode, rphi_ratio2*rMode])
 
 def tripleResonance(rMode, thetaMode, phiMode, a, e, is_prograde=True):
     """Find (p, x) from resonant modes
@@ -656,7 +808,6 @@ def tripleResonance(rMode, thetaMode, phiMode, a, e, is_prograde=True):
     rphi_ratio = phiMode/rMode
     Lz_sign = int(is_prograde)*2-1
     
-    
     if not valid_frequencyRatio(rtheta_ratio):
         raise ValueError("Require r-mode > theta-mode > 0")
     if not valid_frequencyRatio(rphi_ratio):
@@ -668,31 +819,25 @@ def tripleResonance(rMode, thetaMode, phiMode, a, e, is_prograde=True):
     if not valid_params(a, e, 0.5):
         raise ValueError("a^2, e and x^2 must be between 0 and 1")  
 
-    p1_rtheta = newton(lambda p: abs(rtheta_frequencyRatio_equatorialLimit(a, p, e, is_prograde))-rtheta_ratio,
-                       6/(1-rtheta_ratio**2))
-    p2_rtheta = newton(lambda p: abs(rtheta_frequencyRatio_polarLimit(a, p, e, is_prograde))-rtheta_ratio,
-                       6/(1-rtheta_ratio**2))
-    p1_rphi = newton(lambda p: abs(rphi_frequencyRatio_equatorialLimit(a, p, e, is_prograde))-rphi_ratio,
-                    6/(1-rphi_ratio**2))
-    p2_rphi = newton(lambda p: abs(rphi_frequencyRatio_polarLimit(a, p, e, is_prograde))-rphi_ratio,
-                    6/(1-rphi_ratio**2))
-
+    sep1 = separatrix(a, e, Lz_sign)
+    sep2 = separatrix(a, e, 0)
+    p0_rtheta = 6/(1-rtheta_ratio**2)
+    p0_rphi = 6/(1-rphi_ratio**2)
+    p1_rtheta = _doubleResonance_solver(lambda p: abs(rtheta_frequencyRatio_equatorialLimit(a, p, e, is_prograde))-rtheta_ratio, p0_rtheta, sep1)
+    p1_rphi = _doubleResonance_solver(lambda p: abs(rphi_frequencyRatio_equatorialLimit(a, p, e, is_prograde))-rphi_ratio, p0_rphi, sep1)
+    p2_rtheta = _doubleResonance_solver(lambda p: abs(rtheta_frequencyRatio_polarLimit(a, p, e, is_prograde))-rtheta_ratio, p0_rtheta, sep2)
+    p2_rphi = _doubleResonance_solver(lambda p: abs(rphi_frequencyRatio_polarLimit(a, p, e, is_prograde))-rphi_ratio, p0_rphi, sep2)    
+                                   
     check_if_having_solution = (p1_rtheta-p1_rphi)*(p2_rtheta-p2_rphi)
-
     
     if (check_if_having_solution > 0) | (check_if_having_solution == nan):
         return [nan, nan]
-
     elif check_if_having_solution < 0:
-        if abs(p1_rtheta-p1_rphi) < abs(p2_rtheta-p2_rphi):
-            p0 = (p1_rtheta+p1_rphi)/2
-        else: 
-            p0 = (p2_rtheta+p2_rphi)/2
-        x0 = Lz_sign*0.5
+        x0 = Lz_sign/((p1_rphi-p1_rtheta)/(p2_rtheta-p2_rphi)+1)
+        p0 = p2_rtheta-Lz_sign*x0*(p2_rtheta-p1_rtheta)
         sol = fsolve(lambda X: [abs(_rtheta_frequencyRatio(a, X[0], e, X[1]))-rtheta_ratio,
                                 abs(_rphi_frequencyRatio(a, X[0], e, X[1]))-rphi_ratio], [p0, x0])
         return sol
-    
     else:
         if (p1_rtheta-p1_rphi) == 0:
             return [p1_rtheta, Lz_sign]
